e4159bac4077564e9c36f26e53d42591d5a9afab
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 22:28:43 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,14 @@ def countdown(start)
   while start > 0
     puts "#{start} SECOND(S)!"
     start -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(start)
+  while start > 0
+    puts "#{start} SECOND(S)!"
+    start -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
